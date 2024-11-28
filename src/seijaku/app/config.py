0674e6bbf3673afda6e226074ccbce04a1595154
Mix <32300164+mnixry@users.noreply.github.com>
2024-11-28 23:17:14 +0800

    Refactor settings configuration to enhance clarity and add support for host, port, log level, and auto-reload options

--- src/seijaku/app/config.py
+++ src/seijaku/app/config.py
@@ -1,24 +1,58 @@
+from datetime import timedelta
+from enum import StrEnum
 from functools import cache
-from typing import Annotated
+from ipaddress import IPv4Address
+from logging import getLevelNamesMapping
+from typing import Annotated, cast
 
 from fastapi import Depends
-from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field, IPvAnyAddress
+from pydantic_settings import BaseSettings, CliImplicitFlag, SettingsConfigDict
+
+LogLevels = StrEnum(
+    "LogLevels",
+    list(getLevelNamesMapping().keys()),
+)
+
+PortNumber = Annotated[int, Field(gt=0, lt=65536)]
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_prefix="SJK_")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_prefix="SJK_", cli_parse_args=True
+    )
 
-    c2_port: int = 2333
-    """Port of the C2 server"""
-    c2_host: str | None = None
-    """Host of the C2 server, if None, will bind to all interfaces"""
-    database_uri: str = "sqlite+aiosqlite:///./db.sqlite3"
-    """URI of the database to use"""
-    encryption_key: str = Field(pattern=r"^[a-f0-9]{64}$")
-    """Key to use for encryption, must be 64 lowercase hex characters"""
-    session_expire: int = 60 * 60 * 24
-    """Session expiration time in seconds"""
+    host: IPvAnyAddress = Field(
+        default=IPv4Address("0.0.0.0"),  # noqa: S104
+        alias="host",
+        description="Host the web server will listen on",
+    )
+    port: PortNumber = Field(
+        default=8000, alias="port", description="Port the web server will listen on"
+    )
+    log_level: LogLevels = Field(
+        default=cast(LogLevels, "info"),
+        alias="log_level",
+        description="Log level",
+    )
+    reload: CliImplicitFlag[bool] = Field(
+        False, alias="reload", description="Enable auto-reload"
+    )
+
+    c2_port: PortNumber = Field(default=2333, description="Port of the C2 server")
+    c2_host: IPvAnyAddress | None = Field(
+        None, description="Host of the C2 server, if None, will bind to all interfaces"
+    )
+    database_uri: str = Field(
+        "sqlite+aiosqlite:///./db.sqlite3", description="URI of the database to use"
+    )
+    encryption_key: str = Field(
+        pattern=r"^[a-f0-9]{64}$",
+        description="Key to use for encryption, must be 64 lowercase hex characters",
+    )
+    session_expire: timedelta = Field(
+        timedelta(days=1), description="Session expiration time"
+    )
 
 
 @cache
